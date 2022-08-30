92b226fe9d84d2d86d71b43019f89565d9ea34ef
Paul Watts <paul.watts@me.com>
2022-08-31 05:55:31 +1000

    Adjust .gitignore for Jetbrains IDE and Convert 004 Loops Control Structure to Clion

--- 004 Loops Control Stucture/Assignment/2_nested_loop.c
+++ 004 Loops Control Stucture/Assignment/2_nested_loop.c
@@ -1,12 +1,11 @@
 /* Solution to Problem_2 Nested Loop, Triangle Variation 1 for Loop Control Structures
 
     Author: Paul Watts
-    Date:   30th August, 2022
+    Date:   30th August 2022
 
 */
 
 #include <stdio.h>
-#include <stdbool.h>
 
 int main()
 {
@@ -16,8 +15,9 @@ int main()
     int limit;     // Initialise the limit variable for the 2nd loop
     int stars = 0; // Initialise the number of stars to print
 
+
     printf("Enter size of triangle: ");
-    scanf("%d", &limit);
+    scanf(" %d", &limit);
 
     for (i; i < limit; i++)
     {
@@ -26,9 +26,9 @@ int main()
             stars++;
             printf("*");
         }
-        printf("\n");
+       printf("\n");
     }
-    printf("Press any key to continue...");
+    printf("Press any key to continue... \n");
     getchar();
     return 0;
 }
