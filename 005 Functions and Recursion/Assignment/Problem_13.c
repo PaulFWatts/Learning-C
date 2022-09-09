363d2381bb3443f7b2933cba3141a70b8eb9cd7a
Paul Watts <7057417+PaulFWatts@users.noreply.github.com>
2022-09-10 05:44:56 +1000

    Initial setup for 005 Functions and Recursion - Assignment

---
+++ 005 Functions and Recursion/Assignment/Problem_13.c
@@ -0,0 +1,22 @@
+/* Solution to Problem: 13 | Guess-the-Number Game 
+
+    Author: Paul Watts
+    Date:   10th September, 2022
+
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+void wait() {
+    printf("Press any key to continue...\n");
+    int ch = getchar();
+    fflush(stdin);
+    ch = getchar();
+}
+
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
