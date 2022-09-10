a19418c0572d7e8b3a3451faa8cf5ca6ae2a5082
PaulFWatts <paul.watts@me.com>
2022-09-10 17:56:25 +1000

    Complete Problem 12 | Factorial using value returning function

--- 005 Functions and Recursion/Assignment/Problem_12.c
+++ 005 Functions and Recursion/Assignment/Problem_12.c
@@ -1,6 +1,40 @@
+/* Solution to Problem: 12 | Factorial using value returning function
+ *
+ * Calculate factorial of a positive integer n, using a value returning factorial
+
+    Author: Paul Watts
+    Date:   10th September 2022
+
+*/
+
+
 #include <stdio.h>
-int main()
+
+int factorial(int n)
 {
-    printf("Hello World!");
+    int i = 0;
+    int result = 1;
+
+    for (i = 1; i <= n; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int main(void)
+{
+    int n = 0;
+    int result = 0;
+
+    printf("Please enter a positive integer: ");
+    scanf("%d", &n);
+
+    result = factorial(n);
+
+    printf("The factorial of %d is %d\n", n, result);
+
     return 0;
 }
+
+
