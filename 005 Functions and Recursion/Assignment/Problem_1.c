7d435d11a105fff4204ae09bda4a250c3076da92
PaulFWatts <paul.watts@me.com>
2022-09-10 20:12:37 +1000

    Complete 005 Functions and Recursion

--- 005 Functions and Recursion/Assignment/Problem_1.c
+++ 005 Functions and Recursion/Assignment/Problem_1.c
@@ -1,6 +1,23 @@
+/* Solution to Problem: 1 | Evaluate the given expressions
+ *
+ * Evaluate the following expressions and display the results:
+
+    Author: Paul Watts
+    Date:   10th September 2022
+
+*/
+
 #include <stdio.h>
-int main()
+
+int main(void)
 {
-    printf("Hello World!");
-    return 0;
+    printf("a) %d\n", 25 /3);
+    printf("b) %d\n", 20 - 12 / 4 * 2);
+    printf("c) %d\n", 32 % 7);
+    printf("d) %d\n", 3 - 5 % 7);
+    printf("e) %f\n", 18.0 / 4);
+    printf("f) %f\n", 28 - 5 / 2.0);
+    printf("g) %d\n", 17 + 5 % 2 - 3);
+    printf("h) %f\n", 15.0 + 3.0 * 2.0 / 5.0);
+
 }
